d968b0d2cdff1dba0951257f574978e9bf0360e1
Dercilio Fontes <dercilioafontes@gmail.com>
2024-11-04 09:18:29 -0500

    Initial tree preorder traversal

---
+++ tree/preorder-traversal.ts
@@ -0,0 +1,30 @@
+class TreeNode {
+  val: unknown;
+  left: TreeNode | null = null;
+  right: TreeNode | null = null;
+
+  constructor(val: unknown) {
+    this.val = val;
+  }
+}
+
+const preorderDfs = (node: TreeNode | null) => {
+  if (!node) {
+    return;
+  }
+
+  console.log(node.val);
+  preorderDfs(node.left);
+  preorderDfs(node.right);
+  return;
+};
+
+const root = new TreeNode(0);
+root.left = new TreeNode(1);
+root.left.left = new TreeNode(3);
+root.left.right = new TreeNode(4);
+root.left.right.right = new TreeNode(6);
+root.right = new TreeNode(2);
+root.right.right = new TreeNode(5);
+
+preorderDfs(root);
